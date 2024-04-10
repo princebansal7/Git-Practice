610c301eec89e0dd3ff2f0c6e009fc1f607bae3c
Prince Bansal <prince.bansal008@gmail.com>
2024-04-11 03:13:27 +0530

    fixed division by 0 error

--- test.js
+++ test.js
@@ -16,6 +16,11 @@ sum = (a, b) => a * b;
 ans = sum(6, 9);
 console.log("multiplication is:", ans);
 
-sum = (a, b) => a / b;
-ans = sum(6, 9);
-console.log("division is:", ans);
+function div(a, b) {
+    if (b == 0) {
+        console.log("CAN'T DIVIDE BY 0");
+        return "NULL";
+    } else return a / b;
+}
+console.log("division is:", div(6, 9));
+console.log("division is:", div(6, 0));
