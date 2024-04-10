cbc35f70d824dd5723675370af8158add5c1005e
Prince Bansal <prince.bansal008@gmail.com>
2024-04-11 03:06:07 +0530

    added -,%,*,/ fxns

--- test.js
+++ test.js
@@ -3,3 +3,19 @@ console.log("Prince Bansal");
 sum = (a, b) => a + b;
 ans = sum(6, 9);
 console.log("sum is:", ans);
+
+sum = (a, b) => a - b;
+ans = sum(6, 9);
+console.log("subtraction is:", ans);
+
+sum = (a, b) => a % b;
+ans = sum(6, 9);
+console.log("remainder is:", ans);
+
+sum = (a, b) => a * b;
+ans = sum(6, 9);
+console.log("multiplication is:", ans);
+
+sum = (a, b) => a / b;
+ans = sum(6, 9);
+console.log("division is:", ans);
